1c2a77cb997111be951afa79a1fb0d0db758c050
wasamilk <vin5004@naver.com>
2022-11-25 17:57:09 +0900

    'setting_router'

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home'
+import About from './About'
+
+export default createRouter({
+    // 1.Hash mode , 2.History mode
+    // mode 1  [ ex) https://google.com/#/search ]
+    history:createWebHashHistory(),
+    // pages
+    routes: [
+        {
+            // 경로
+            path:'/',
+            component: Home
+        },
+        {
+            path:'/about',
+            component: About
+        }
+    ]
+})
